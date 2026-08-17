1ccd9463e6c19d14af83719365adedcc20adad24
Saketh Sibyala <saketh.sibyala@gmail.com>
2026-08-18 00:59:18 +0530

    A1 and A2

--- c-shell/src/main.c
+++ c-shell/src/main.c
@@ -1,7 +1,41 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
+#include <string.h>
+#include <limits.h>
 
-int main(void)
+void printPath()
 {
-    printf("hello\n");
-    return 0;
+    char username[256];
+    char hostname[256];
+    char path[PATH_MAX];
+
+    uid_t uid = getuid();
+    struct passwd *pw = getpwuid(uid);
+
+    strcpy(username, pw->pw_name);
+    gethostname(hostname, sizeof(hostname));
+    getcwd(path, sizeof(path));
+
+    char *home = pw->pw_dir;
+
+    if(strncmp(path, home, strlen(home)) == 0)
+    {
+        printf("%s@%s:~%s$ ", username, hostname, path + strlen(home));
+    }
+    else
+    {
+        printf("%s@%s:%s$ ", username, hostname, path);
+    }
+}
+
+int main()
+{
+    while(1)
+    {
+        printPath();
+        char str[256];
+        fgets(str, sizeof(str), stdin);
+    }
 }
